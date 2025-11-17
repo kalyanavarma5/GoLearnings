e7477f22cb99bed0d52e90cdb7d6c0541fc9635c
Kalyana Varma <>
2025-11-17 13:15:55 +0530

    Added hello world

---
+++ HelloWorld/hello.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello MY World")
+	variables()
+}
---
+++ HelloWorld/variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func variables() {
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+
+	var king2 = false
+	king3 := 3457348534
+	fmt.Println(i, j, k, c, python, java, king2, king3)
+}
